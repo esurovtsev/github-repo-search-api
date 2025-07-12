73a95be63c6797b37c0b758ef25d94559ec81360
esurovtsev <2109127+esurovtsev@users.noreply.github.com>
2025-07-12 15:02:22 +0200

    added main application class

---
+++ src/main/java/com/grabduck/githubsearch/GithubRepoSearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.grabduck.githubsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepoSearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRepoSearchApiApplication.class, args);
+	}
+
+}
